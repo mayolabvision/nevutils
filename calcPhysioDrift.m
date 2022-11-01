a8b556f47d9219f0b874fd0e070d5d115446894c
e-c-crane <98123604+e-c-crane@users.noreply.github.com>
2022-11-01 14:57:52 -0400

    Update calcPhysioDrift.m
    changed output to include both the extracted trace and the binned trace, added optional parameter controlling whether or not figures are plotted

--- calcPhysioDrift.m
+++ calcPhysioDrift.m
@@ -1,8 +1,13 @@
-function [physioDrift, t_runavg] = calcPhysioDrift(physioDat, fs, varargin)
+function [physioTrace, t, physioDrift, t_runavg] = calcPhysioDrift(physioDat, fs, varargin)
 % calcPhysioDrift extracts the rate of a physiologic oscillation (ex. cardiac pulses in NIRS,
-% respiration in thermal sesnor signal) and creates a binned rate trace over the session that 
+% respiration in thermal sensor signal) and creates a binned rate trace over the session that 
 % can be compared to slow drift.
-%   - physioDat: 1D physiologic data trace
+%   - physioTrace: extracted rate trace
+%   - t: time vector corresponding to physioTrace
+%   - physioDrift: Running avg 1D physiologic data trace (unless
+%                  ~runAvgFlag).
+%   - t_runavg: Time vector corresponding to binned data trace. Samples
+%               have been thinned due to binning.
 %   - fs: data sampling rate
 %   - physioRange: 2 element frequency range in Hz for expected rate of the
 %                  physiologic signal (ex. respiration b/w 0.15 and 0.9
@@ -26,6 +31,7 @@ p.addOptional('penalty',0.001,@isscalar);
 p.addOptional('runAvgFlag', true, @islogical)
 p.addOptional('binL', 30*60, @isscalar);
 p.addOptional('shiftL', 6*60, @isscalar);
+p.addParameter('plotFigs', true, @islogical)
 
 p.parse(varargin{:});
 
@@ -37,6 +43,7 @@ penalty = p.Results.penalty;
 runAvgFlag = p.Results.runAvgFlag;
 binL = p.Results.binL;
 shiftL = p.Results.shiftL;
+plotFigs = p.Results.plotFigs;
 
 %% (1) ********* Extract rate across session ******************
 windowSize = specWin*fs; %in samples
@@ -46,41 +53,43 @@ windowSize = specWin*fs; %in samples
 [b,a]= butter(2,physioRange/(fs/2),'bandpass'); % narrowband filter the trace
 datFilt = filtfilt(b,a,physioDat);
 
-% plot for visualization
-figure; spectrogram(datFilt,windowSize,round(windowSize*specOverlap),[],fs,'yaxis');
-ylim([physioRange(1)/2 3*physioRange(2)/2]); hold on;
 
 switch extractMethod
     case "spectrogram"
         % compute spectrogram of data trace and extract from trace
-        [physio_trace,t] = extractSpecFreq(physioDat,fs,physioRange,windowSize,specOverlap,'penalty',penalty);
+        [physioTrace,t] = extractSpecFreq(physioDat,fs,physioRange,windowSize,specOverlap,'penalty',penalty);
 
     case "peaks"
         % use findpeaks on trace
         [~,pkloc] = findpeaks(datFilt,'MinPeakDistance',fs/physioRange(2));
         t = pkloc./fs; % convert from samples to time
-        physio_trace = 1./diff(t); % time difference between adjacent peaks to frequency in Hz
+        physioTrace = 1./diff(t); % time difference between adjacent peaks to frequency in Hz
         t = t(1:end-1);
 
     otherwise
         error('undefined method for extracting rate trace');
 end
 
-% plot rate trace over spectrogram of data trace
-plot(t/3600,physio_trace,'k');
+if plotFigs
+    % plot for visualization
+    figure; spectrogram(datFilt,windowSize,round(windowSize*specOverlap),[],fs,'yaxis');
+    ylim([physioRange(1)/2 3*physioRange(2)/2]); hold on;
+    % plot rate trace over spectrogram of data trace
+    plot(t/3600,physioTrace,'k');
+end
 
-%% (3) ********* Bin data across the session *****************************
+%% (2) ********* Bin data across the session *****************************
 
 if strcmp("spectrogram",extractMethod) && ~runAvgFlag
     % if spectrogram method and running average flag is false then just use the
     % trace extracted from the spectrogram as the physiological drift output
     t_runavg = t;
-    physioDrift = physio_trace;
+    physioDrift = physioTrace;
 
 else
     % otherwise bin the rate trace in the same way as the slow drift data 
     t = t - t(1); % time bins for physiological data should be identifical to slow drfit (starting at 0)
-    [ t_runavg, physioDrift, ~] = temporalRunAvg(t,physio_trace,binL,shiftL);
+    [ t_runavg, physioDrift, ~] = temporalRunAvg(t,physioTrace,binL,shiftL);
 end
 
 end
